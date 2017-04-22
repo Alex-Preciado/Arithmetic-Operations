c35f246a7d35747271822ae63d19ac868288da84
Jorge Preciado <alexpreciado1@gmail.com>
2017-04-22 05:35:32 -0400

    Updated documentation files

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['main',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
-  ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]],
-  ['multiply',['multiply',['../class_arithmetic.html#a08401cbe81a8a58ca88f648c1f297e64',1,'Arithmetic']]]
+  ['divide',['divide',['../class_arithmetic.html#a262e0de1fd06b5fcba23a1ce81a61988',1,'Arithmetic']]]
 ];
--- src/arithmetic.h
+++ src/arithmetic.h
@@ -1,3 +1,27 @@
+//! \file arithmetic.cpp
+//!
+//! \author Alex Preciado, Someone Else
+///
+//! \brief This is the implementation file for the Arithmetic Class
+///
+//! This file containts the implementation for the addition, substraction,
+/// multiplication and division functions of the Arithmetic class.
+
+
+/// \brief My class
+///
+/// Longer description of this class
+/// in multiple lines.
+///
+/// \bug The overload function "operator" and the multiplication functions
+/// performe the exact same process
+///
+/// \todo We have to modify the implemntation of the overload function "operator"
+///
+/// \todo (Alex) You have to do this too...
+
+
+
 #ifndef ARITHMETIC_H_
 #define ARITHMETIC_H_
 
